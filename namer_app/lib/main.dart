ea9e08d24578508711427ebd125b45520b0fbef8
Takuma Homma <nagomimatcha@gmail.com>
2024-08-01 15:31:46 +0900

    Add state to manage current word

--- namer_app/lib/main.dart
+++ namer_app/lib/main.dart
@@ -25,8 +25,14 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// screen state
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNext() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
@@ -37,8 +43,13 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          Text('A random idea:'),
+          Text('A random AWESOME idea:'),
           Text(appState.current.asLowerCase),
+          ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next')),
         ],
       ),
     );
