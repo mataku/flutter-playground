97b0d1d9bc44b7a8c78332e1254c1d1cd3b5b490
Takuma Homma <nagomimatcha@gmail.com>
2024-08-18 00:08:41 +0900

    Add chart endpoints

--- state_app/test/fixtures/test_data.dart
+++ state_app/test/fixtures/test_data.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:state_app/api/response/common_text_response.dart';
 import 'package:state_app/api/response/image_response.dart';
 import 'package:state_app/api/response/recent_tracks_api_response.dart';
@@ -33,3 +34,10 @@ const extraLargeArtwork =
     Artwork(size: "extralarge", url: "https://example.com/extralarge.png");
 const largeArtwork =
     Artwork(size: "large", url: "https://example.com/large.png");
+
+final _dioOptions = BaseOptions(
+  connectTimeout: const Duration(seconds: 5),
+  receiveTimeout: const Duration(seconds: 3),
+  queryParameters: {'format': 'json'},
+);
+final testDio = Dio(_dioOptions);
