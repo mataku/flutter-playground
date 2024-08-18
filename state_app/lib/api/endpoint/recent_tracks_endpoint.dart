774a8025329692829f70f82b8de548622ebbffad
Takuma Homma <nagomimatcha@gmail.com>
2024-08-18 22:34:59 +0900

    Squash

--- state_app/lib/api/endpoint/recent_tracks_endpoint.dart
+++ state_app/lib/api/endpoint/recent_tracks_endpoint.dart
@@ -3,10 +3,11 @@ import 'package:state_app/api/endpoint/endpoint.dart';
 import 'package:state_app/api/response/recent_tracks_api_response.dart';
 
 class RecentTracksEndpoint extends Endpoint<RecentTracksApiResponse> {
-  RecentTracksEndpoint(
-      {super.path = '/2.0/?method=user.getrecenttracks',
-      required super.params,
-      super.requestType = RequestType.get});
+  RecentTracksEndpoint({
+    super.path = '/2.0/?method=user.getrecenttracks',
+    required super.params,
+    super.requestType = RequestType.get,
+  });
 
   @override
   RecentTracksApiResponse parseFromJson(Response response) {
