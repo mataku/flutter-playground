fb8df97882bbaa279397d4e925c39f28c3f51990
Takuma Homma <nagomimatcha@gmail.com>
2024-08-18 18:56:57 +0900

    Prepare sample responses for chart resources

--- state_app/lib/repository/chart_repository.dart
+++ state_app/lib/repository/chart_repository.dart
@@ -1,8 +1,15 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:state_app/api/endpoint/chart_top_artists_endpoint.dart';
 import 'package:state_app/api/endpoint/chart_top_tags_endpoint.dart';
 import 'package:state_app/api/endpoint/chart_top_tracks_endpoint.dart';
 import 'package:state_app/api/last_fm_api_service.dart';
+import 'package:state_app/api/response/chart_top_artists_api_response.dart';
+import 'package:state_app/api/response/chart_top_tags_api_response.dart';
+import 'package:state_app/api/response/chart_top_tracks_api_response.dart';
 import 'package:state_app/api/response/mapper/response_mapper.dart';
 import 'package:state_app/model/app_error.dart';
 import 'package:state_app/model/chart_artist.dart';
@@ -12,9 +19,16 @@ import 'package:state_app/model/tag.dart';
 
 abstract class ChartRepository {
   Future<Result<List<ChartArtist>>> getChartArtists(int page);
+  Future<Result<List<ChartArtist>>> getChartArtistsSample(int page);
+
   Future<Result<List<ChartTrack>>> getChartTracks(int page);
+  Future<Result<List<ChartTrack>>> getChartTracksSample(int page);
   Future<Result<List<Tag>>> getChartTags(int page);
+  Future<Result<List<Tag>>> getChartTagsSample(int page);
 }
+
+final chartRepositoryProvider = Provider((ref) =>
+    ChartRepositoryImpl(lastFmApiService: ref.read(lastFmApiServiceProvider)));
 
 class ChartRepositoryImpl implements ChartRepository {
   final LastFmApiService lastFmApiService;
@@ -39,6 +53,15 @@ class ChartRepositoryImpl implements ChartRepository {
   }
 
   @override
+  Future<Result<List<ChartArtist>>> getChartArtistsSample(int page) async {
+    final data =
+        await rootBundle.loadString("asset/json/chart_top_artists.json");
+    final result = json.decode(data) as Map<String, dynamic>;
+    final artists = ChartTopArtistsApiResponse.fromJson(result);
+    return Result.success(artists.toChartArtistList());
+  }
+
+  @override
   Future<Result<List<ChartTrack>>> getChartTracks(int page) async {
     final endpoint = ChartTopTracksEndpoint(
       params: {
@@ -56,6 +79,15 @@ class ChartRepositoryImpl implements ChartRepository {
   }
 
   @override
+  Future<Result<List<ChartTrack>>> getChartTracksSample(int page) async {
+    final data =
+        await rootBundle.loadString("asset/json/chart_top_tracks.json");
+    final result = json.decode(data) as Map<String, dynamic>;
+    final tracks = ChartTopTracksApiResponse.fromJson(result);
+    return Result.success(tracks.toChartTrackList());
+  }
+
+  @override
   Future<Result<List<Tag>>> getChartTags(int page) async {
     final endpoint = ChartTopTagsEndpoint(
       params: {
@@ -70,5 +102,13 @@ class ChartRepositoryImpl implements ChartRepository {
       debugPrint("MATAKUDEBUG $error");
       return Result.failure(AppError.getApiError(error));
     }
+  }
+
+  @override
+  Future<Result<List<Tag>>> getChartTagsSample(int page) async {
+    final data = await rootBundle.loadString("asset/json/chart_top_tags.json");
+    final result = json.decode(data) as Map<String, dynamic>;
+    final tags = ChartTopTagsApiResponse.fromJson(result);
+    return Result.success(tags.toTagList());
   }
 }
