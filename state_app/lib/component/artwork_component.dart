b63537143d9fef47f50fbcd2b02d5e563bf647cc
Takuma Homma <nagomimatcha@gmail.com>
2024-08-20 23:47:35 +0900

    Square artwork

--- state_app/lib/component/artwork_component.dart
+++ state_app/lib/component/artwork_component.dart
@@ -4,8 +4,11 @@ class ArtworkComponent extends StatelessWidget {
   final String? imageUrl;
   final double size;
 
-  const ArtworkComponent(
-      {super.key, required this.imageUrl, required this.size});
+  const ArtworkComponent({
+    super.key,
+    required this.imageUrl,
+    required this.size,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +31,42 @@ class ArtworkComponent extends StatelessWidget {
 
     return SizedBox(
       height: size,
+      width: size,
+      child: imageComponent,
+    );
+  }
+}
+
+class ArtworkSquareComponent extends StatelessWidget {
+  final String? imageUrl;
+  final double size;
+
+  const ArtworkSquareComponent({
+    super.key,
+    required this.imageUrl,
+    required this.size,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final Widget imageComponent;
+
+    if (imageUrl == null || imageUrl?.isEmpty == true) {
+      imageComponent = Image.asset(
+        "asset/image/no_image.png",
+        fit: BoxFit.cover,
+      );
+    } else {
+      imageComponent = Image.network(imageUrl!, fit: BoxFit.cover,
+          errorBuilder: (BuildContext context, error, stacktrace) {
+        return Image.asset(
+          "asset/image/no_image.png",
+          fit: BoxFit.cover,
+        );
+      });
+    }
+
+    return SizedBox(
       width: size,
       child: imageComponent,
     );
