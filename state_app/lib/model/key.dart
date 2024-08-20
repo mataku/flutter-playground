19cf73a7ed4319b194403c6cdbf40e6eea894ba7
Takuma Homma <nagomimatcha@gmail.com>
2024-08-20 13:07:32 +0900

    Specify path explicitly

--- state_app/lib/model/key.dart
+++ state_app/lib/model/key.dart
@@ -2,11 +2,11 @@ import 'package:envied/envied.dart';
 
 part 'key.g.dart';
 
-@envied
+@envied(path: '.env')
 abstract class Env {
-  @EnviedField(varName: 'API_KEY')
+  @EnviedField(varName: 'API_KEY', obfuscate: true)
   static const String apiKey = _Env.apiKey;
 
-  @EnviedField(varName: 'SHARED_SECRET')
+  @EnviedField(varName: 'SHARED_SECRET', obfuscate: true)
   static const String sharedSecret = _Env.sharedSecret;
 }
