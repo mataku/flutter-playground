27ebf36a68200b00be1a002d6bd81da172304278
Takuma Homma <nagomimatcha@gmail.com>
2024-08-24 23:33:46 +0900

    Mapper for TopArtistsApiResponse

---
+++ lastfm_app/lib/model/user/top_artist.dart
@@ -0,0 +1,14 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:state_app/model/artwork.dart';
+
+part 'top_artist.freezed.dart';
+
+@freezed
+class TopArtist with _$TopArtist {
+  const factory TopArtist({
+    required String name,
+    required String url,
+    required String playcount,
+    required List<Artwork> images,
+  }) = _TopArtist;
+}
