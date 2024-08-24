c08063b261b4fa1ada5a9ce52f3353c7c23da0f9
Takuma Homma <nagomimatcha@gmail.com>
2024-08-24 23:37:06 +0900

    Add UserTopArtistsEndpoint

---
+++ lastfm_app/test/api/endpoint/user_top_artists_endpoint_test.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http_mock_adapter/http_mock_adapter.dart';
+import 'package:state_app/api/endpoint/user_top_artists_endpoint.dart';
+import 'package:state_app/api/last_fm_api_service.dart';
+
+import '../../fixture.dart';
+import '../../fixtures/test_data.dart';
+
+void main() {
+  group('UserTopArtistsEndpoint', () {
+    late Dio dio;
+    late DioAdapter dioAdapter;
+    late LastFmApiService apiService;
+
+    setUp(() {
+      dio = testDio;
+      dioAdapter = DioAdapter(dio: dio);
+      apiService = LastFmApiService(dio: dio);
+    });
+
+    test('should request with expected params', () async {
+      final response = fixture('user_top_artists.json');
+      dioAdapter.onGet(
+        '/2.0/?method=user.gettopartists',
+        (server) => server.reply(200, jsonDecode(response)),
+        data: null,
+        queryParameters: {
+          'format': 'json',
+          'page': '1',
+        },
+        headers: {},
+      );
+      final endpoint = UserTopArtistsEndpoint(params: {'page': '1'});
+      final result = await apiService.request(endpoint);
+      expect(result.response.artists.isNotEmpty, true);
+    });
+  });
+}
