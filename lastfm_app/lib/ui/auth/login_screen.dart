1b4fbe9b31c5975919076ff199a47930a1d11694
Takuma Homma <nagomimatcha@gmail.com>
2024-08-26 23:23:41 +0900

    Add username and password forms

--- lastfm_app/lib/ui/auth/login_screen.dart
+++ lastfm_app/lib/ui/auth/login_screen.dart
@@ -8,7 +8,31 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Text('login!!'),
+      body: Padding(
+        padding: EdgeInsets.only(left: 16, right: 16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Username',
+              ),
+              keyboardType: TextInputType.visiblePassword,
+            ),
+            Padding(padding: EdgeInsets.only(top: 16)),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.visiblePassword,
+              obscureText: true,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
