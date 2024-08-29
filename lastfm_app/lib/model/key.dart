863c87540459ec45ca1ce0bd60e4db4b086dca4f
Takuma Homma <nagomimatcha@gmail.com>
2024-08-29 22:06:10 +0900

    final

--- lastfm_app/lib/model/key.dart
+++ lastfm_app/lib/model/key.dart
@@ -5,8 +5,8 @@ part 'key.g.dart';
 @envied
 abstract class Env {
   @EnviedField(varName: 'API_KEY', obfuscate: true)
-  static String apiKey = _Env.apiKey;
+  static final String apiKey = _Env.apiKey;
 
   @EnviedField(varName: 'SHARED_SECRET', obfuscate: true)
-  static String sharedSecret = _Env.sharedSecret;
+  static final String sharedSecret = _Env.sharedSecret;
 }
