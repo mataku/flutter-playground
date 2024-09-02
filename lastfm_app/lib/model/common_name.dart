0398e691212672a950d69d41ee34b03c9a798284
Takuma Homma <nagomimatcha@gmail.com>
2024-09-02 19:10:31 +0900

    Delete old dir

--- lastfm_app/lib/model/common_name.dart
+++
@@ -1,8 +0,0 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'common_name.freezed.dart';
-
-@freezed
-class CommonName with _$CommonName {
-  const factory CommonName({required String name}) = _CommonName;
-}
--- lastfm_app/test/fixture.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-
-String fixture(String name) {
-  File file = File('${Directory.current.path}/test/fixtures/$name');
-
-  return file.readAsStringSync();
-}
